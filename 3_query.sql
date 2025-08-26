c2b5d6566d175aa10c0eba3484e9132e8aa15e49
Egor Petrov <egor.petrov@imms.de>
2025-08-26 18:47:57 +0300

    Изменен запрос для 3 задания

--- 3_query.sql
+++ 3_query.sql
@@ -1,7 +1,5 @@
 SELECT 
     card_type,
-    COUNT(*) AS total_cards,
-    COUNT(*) FILTER (WHERE churn_date IS NOT NULL) AS churned_cards,
     ROUND(COUNT(*) FILTER (WHERE churn_date IS NOT NULL) * 100.0 / COUNT(*), 0) AS churn_percent
 FROM cards
 GROUP BY card_type;
